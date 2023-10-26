2a753313c9e91d26afbf438d707909eab53ebeff
Cauã Rufino de Sá <127906505+CauaDeSa@users.noreply.github.com>
2023-10-26 18:43:14 -0300

    Add files via upload

--- Aula 9. Projeto Site de Filmes (API, Bootstrap)/js/script.js
+++ Aula 9. Projeto Site de Filmes (API, Bootstrap)/js/script.js
@@ -1,9 +1,15 @@
 var element = document.querySelector("html");
 var setToDark = document.querySelector("#setColor");
 var setNav = document.querySelectorAll('.nav-link');
-var changePage = document.querySelectorAll('#navButton');
+var changePage = document.querySelectorAll('.pageSetter');
 var cardSpace = document.querySelector('.row');
+var searchLabel = document.querySelector('.cabecalho-pesquisa');
+var tituloPagina = document.querySelector('.pageTitle');
+var cardsQuantity = 0;
 var isDark = true;
+
+find();
+tituloPagina.textContent = 'Mehores avaliados';
 
 setToDark.addEventListener("click", () => {
     if (!isDark) {
@@ -23,24 +29,50 @@ setToDark.addEventListener("click", () => {
         });
         isDark = false;
     }
+});
+
+searchLabel.addEventListener("keydown", function (e) {
+
+    if (e.code === "Enter") {
+
+        clear();
+
+        let word = searchLabel.value;
+        let title = document.createElement('div');
+        let titleContent = document.createElement('p');
+        titleContent.textContent = 'Voce pesquisou por: "' + word + '"';
+        title.setAttribute('class', 'pageTitle');
+        cardSpace.appendChild(title);
+        title.appendChild(titleContent);
+
+        searchLabel.value = '';
+
+        find('https://api.themoviedb.org/3/search/multi?query=' + word + '&include_adult=false&language=pt-BR&page=1');
+    }
 })
 
 changePage.forEach(function (botao) {
 
     botao.addEventListener('click', () => {
-        if (botao.textContent == 'Home') {
-            find("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1");
+        if (botao.textContent == 'Lançamentos') {
+            clear();
+            tituloPagina.textContent = 'Lançamentos!';
+            find('https://api.themoviedb.org/3/movie/now_playing?language=pt-BR&page=1', 1);
         }
         else if (botao.textContent == 'Filmes') {
-            find("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1");
+            clear();
+            tituloPagina.textContent = 'Melhor avaliados do momento';
+            find('https://api.themoviedb.org/3/movie/upcoming?language=pt-BR&page=1', 2);
         }
         else {
-
+            clear();
+            tituloPagina.textContent = 'Melhor avaliadas do momento';
+            find("https://api.themoviedb.org/3/tv/top_rated?language=pt-BR&page=1", 3);
         }
     })
 });
 
-async function find(url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1') {
+async function find(url = 'https://api.themoviedb.org/3/movie/top_rated?language=pt-BR&page=1', key) {
 
     const options = {
         method: 'GET',
@@ -54,20 +86,83 @@ async function find(url = 'https://api.themoviedb.org/3/movie/top_rated?language
     const data = await dataBlock.json();
     const ore = await data.results;
 
-    ore.forEach(movie => {
+    ore.forEach(element => {
         var card = document.createElement('div');
-        var enderecoImg = 'https://image.tmdb.org/t/p/original' + movie.poster_path;
         var backgroundImage = document.createElement('img');
         var title = document.createElement('p');
 
-        card.style.backgroundImage = `url(${enderecoImg})`;
-        // backgroundImage.setAttribute('src', 'https://image.tmdb.org/t/p/original' + movie.poster_path);
+        if (element.poster_path != null) {
+            backgroundImage.setAttribute('src', 'https://image.tmdb.org/t/p/original' + element.poster_path);
+            backgroundImage.setAttribute('class', 'img-card');
 
-        title.textContent = movie.original_title;
-        card.setAttribute('class', 'card');
-        card.appendChild(title);
-        card.appendChild(backgroundImage);
-        cardSpace.appendChild(card);
+            card.setAttribute('class', 'card');
+            card.appendChild(backgroundImage);
+            card.appendChild(title);
+            cardSpace.appendChild(card);
+            card.setAttribute('id', element.id);
+            cardsQuantity++;
+        }
     })
+
+    var description = document.querySelectorAll('.card');
+    var count = 0;
+
+    description.forEach(function descricao(selecionado) {
+        selecionado.addEventListener('click', () => {
+            clear();
+            openList('https://api.themoviedb.org/3/movie/' + selecionado.id + '?language=pt-BR');
+        })
+    });
 }
 
+function clear() {
+    if (cardsQuantity > 0) {
+
+        document.querySelector('.row').innerHTML = '';
+    }
+}
+
+async function openList(url) {
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MGQ5ZGFjYzAwNjA5NmM0YzcwYWM0NGViNmUxM2VmNSIsInN1YiI6IjY1MWYzY2Q1OTY3Y2M3MzQyN2YzZjM4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ImorXfIr9sQNG0vBOP5slVmaT52N8kKl1zmfh5CnOUg'
+        }
+    };
+
+    fetch(url, options)
+    .then(response => response.json())
+    .then(response => {
+        let firstDiv = document.createElement('div');
+        firstDiv.setAttribute('class', 'flex-row');
+
+        cardSpace.appendChild(firstDiv);
+
+        let movieImg = document.createElement('img');
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/original' + response.poster_path);
+        
+        firstDiv.appendChild(movieImg);
+
+        let secondDiv = document.createElement('div');
+        secondDiv.setAttribute('class', 'text-overview');
+        
+        firstDiv.appendChild(secondDiv);
+
+        let movieOverview = document.createElement('p');
+        let vote = document.createElement('p');
+        let date = document.createElement('p');
+
+        tituloPagina.textContent = response.original_title;
+        movieOverview.textContent = 'Visão geral: ' + response.overview;
+        vote.textContent = 'Nota Média: ' + response.vote_average;
+        date.textContent = 'Data de lançamento: ' + response.release_date;
+
+        secondDiv.appendChild(movieOverview);
+        secondDiv.appendChild(vote);
+        secondDiv.appendChild(date);
+    })
+    .catch(err => console.error(err));
+
+}
